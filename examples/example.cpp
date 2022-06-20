e883bac1555e19dc6b3d3a077afb5d0bd5f11e70
Rasmus <jmtateossian50b@gmail.com>
2022-06-20 15:31:06 +0100

    made examples more useful

--- examples/example.cpp
+++ examples/example.cpp
@@ -3,7 +3,8 @@
 
 int main() {
   lodge::info("Hello {}", "World");
-  lodge::error("Hello {}", "World");
-  lodge::warn("Hello {}", "World");
+  lodge::error("Hello {}, {} {}", "World", 3, "arguments");
+  // Should not print "World"
+  lodge::warn("Hello ", "World");
   lodge::debug("THIS IS A SINGLE PARAMETER");
 }
