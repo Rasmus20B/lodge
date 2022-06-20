f987edc4378b6543a1a4dd35aabe6488cd758ebb
Rasmus <jmtateossian50b@gmail.com>
2022-06-20 11:03:20 +0100

    static cast + const constexpr for 'to_string()' function

--- src/levels.hpp
+++ src/levels.hpp
@@ -8,11 +8,11 @@
 
 namespace lodge {
 enum class Level {
-  LODGE_INFO = 0,
-  LODGE_DEBUG = 1,
-  LODGE_WARN = 2,
-  LODGE_ERROR = 3,
-  LODGE_COUNT = 4
+  LODGE_INFO,
+  LODGE_DEBUG,
+  LODGE_WARN,
+  LODGE_ERROR,
+  LODGE_COUNT
 };
 
 constexpr std::string_view LODGE_NAME_INFO = std::string_view("INFO", 4);
@@ -20,10 +20,12 @@ constexpr std::string_view LODGE_NAME_DEBUG = std::string_view("DEBUG", 5);
 constexpr std::string_view LODGE_NAME_WARNING = std::string_view("WARNING", 7);
 constexpr std::string_view LODGE_NAME_ERROR = std::string_view("ERROR", 5);
 
-constexpr std::array<std::string_view, 4> LODGE_LEVEL_NAMES = {
-    LODGE_NAME_INFO, LODGE_NAME_DEBUG, LODGE_NAME_WARNING, LODGE_NAME_ERROR};
+constexpr std::array<std::string_view,
+                     static_cast<std::size_t>(Level::LODGE_COUNT)>
+    LODGE_LEVEL_NAMES = {LODGE_NAME_INFO, LODGE_NAME_DEBUG, LODGE_NAME_WARNING,
+                         LODGE_NAME_ERROR};
 
-inline const std::string_view &to_string_view(Level level) noexcept {
-  return LODGE_LEVEL_NAMES[static_cast<int>(level)];
+inline const constexpr std::string_view &to_string_view(Level level) noexcept {
+  return LODGE_LEVEL_NAMES.at(static_cast<int>(level));
 }
 } // namespace lodge
