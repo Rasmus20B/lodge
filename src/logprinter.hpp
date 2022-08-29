c80ee0cb99c62df415c6c64e6d8789763abd30b2
merimak <jmtateossian50b@gmail.com>
2022-08-29 15:44:54 +0000

    removed unused files

--- src/logprinter.hpp
+++
@@ -1,7 +0,0 @@
-#include "log_item.hpp"
-namespace lodge {
-
-class Printer {
-  Printer() noexcept = default;
-};
-} // namespace lodge
--- src/source.hpp
+++
@@ -1,3 +0,0 @@
-#pragma once
-
-#include <experimental/source_location>
